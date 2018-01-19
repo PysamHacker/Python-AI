68e5845b4522c4574df6eef41f22aba6ac58509d
adi0229 <jiandi.huang@gmail.com>
2018-01-19 16:31:40 +0800

    Create print.py

---
+++ PythonBasic/print.py
@@ -0,0 +1,11 @@
+print('hello world')
+print("hello world 2")
+print('Hello world'+' Hello Hong Kong')
+print(1+1)
+print(3-1)
+print(3*4)
+print(12/4)
+print('iphone'+4) #字符串不可以直接和数字相加
+print(int('2')+3) #int为定义整数型
+print(int(1.9))  #当int一个浮点型数时，int会保留整数部分
+print(float('1.2')+3) #float()是浮点型，可以把字符串转换成小数
