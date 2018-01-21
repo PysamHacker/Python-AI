3fcfd5f585dd33a4c7b4c9930d20c7c4ea8379f9
adi0229 <jiandi.huang@gmail.com>
2018-01-21 19:23:29 +0800

    Create ex1.py

---
+++ lphw/ex1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# for study drill
+
+print("Hello world!")
+print("Hello again!")
+
+print("I like tying this.")
+print("THis is fun.")
+print("I'd much rather you 'not'")
+print('I "said" do not touch this.')
+print("It's another line for Study Drill.")
+print("你好啊，世界!")
+
